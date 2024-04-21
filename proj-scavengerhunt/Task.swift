528e6187390d8cfbfd845c8e6ecd35b75bb07ef3
Abdul Nafay Saleem <nafaysaleem12@gmail.com>
2024-04-21 11:50:36 -0400

    updated ui

--- proj-scavengerhunt/Task.swift
+++ proj-scavengerhunt/Task.swift
@@ -12,16 +12,18 @@ class Task {
     let title: String
     let description: String
     var bgimage: URL?
+    let difficulty: String
     var image: UIImage?
     var imageLocation: CLLocation?
     var isComplete: Bool {
         image != nil
     }
 
-    init(title: String, description: String, bgimage: URL? = nil) {
+    init(title: String, description: String, bgimage: URL? = nil, difficulty: String) {
         self.title = title
         self.description = description
         self.bgimage = bgimage
+        self.difficulty = difficulty
     }
 
     func set(_ image: UIImage, with location: CLLocation) {
@@ -34,10 +36,12 @@ extension Task {
     static var mockedTasks: [Task] {
         return [
             Task(title: "Washington Monument",
-                 description: "Take a selfie near the Washington Monument", bgimage: URL(string:"https://i.postimg.cc/59rHXb1d/Rectangle-1-1.png")),
+                 description: "Take a selfie near the Washington Monument", bgimage: URL(string:"https://i.postimg.cc/59rHXb1d/Rectangle-1-1.png"), difficulty: "Easy"),
             Task(title: "Museum of Natural History",
-                 description: "Visit the Smithsonian National Museum of Natural History and take selfies with the giant elephants!", bgimage: URL(string:"https://i.ibb.co/7NCFvJq/Rectangle-8.png")),
-            
+                 description: "Visit the Smithsonian National Museum of Natural History and take selfies with the giant elephants!", bgimage: URL(string:"https://i.ibb.co/7NCFvJq/Rectangle-8.png"), difficulty: "Medium"),
+            Task(title: "Lincoln Memorial",
+                             description: "Pay a visit to the Lincoln Memorial and learn about American history.",
+                             bgimage: URL(string: "https://i.ibb.co/kSsbNjF/Rectangle-8-1.png"), difficulty: "Hard")
         ]
     }
 }
--- proj-scavengerhunt/TaskCell.swift
+++ proj-scavengerhunt/TaskCell.swift
@@ -16,10 +16,23 @@ class TaskCell: UITableViewCell {
     @IBOutlet var descLabel: UILabel!
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var bgImage: UIImageView!
+    @IBOutlet var difficultyLabel: UIImageView!
     
     func configure(with task: Task) {
         titleLabel.text = task.title
         descLabel.text = task.description
+        
+        
+        switch task.difficulty {
+        case "Easy":
+            difficultyLabel.image = UIImage(named: "easy")
+        case "Medium":
+            difficultyLabel.image = UIImage(named: "med")
+        case "Hard":
+            difficultyLabel.image = UIImage(named: "hard")
+        default:
+            difficultyLabel.image = UIImage(named: "TravelDC")
+        }
         
         let imageURL: URL = task.bgimage ?? URL(string: "https://salonlfc.com/wp-content/uploads/2018/01/image-not-found-1-scaled-1150x647.png")!
            Nuke.loadImage(with: imageURL, into: bgImage)
